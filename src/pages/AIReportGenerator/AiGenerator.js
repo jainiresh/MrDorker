2d3665693ea378050e719470b0541c3afb890782
praveen12191 <haripraveen816@gmail.com>
2025-01-08 19:37:25 +0530

    aiGenerator style

--- src/pages/AIReportGenerator/AiGenerator.js
+++ src/pages/AIReportGenerator/AiGenerator.js
@@ -6,6 +6,7 @@ import Modal from "../../components/Modal/Modal";
 import Loader from "../../components/Loader/Loader";
 import ReportModal from "../../components/Modal/ReportModal";
 import { toast } from "react-toastify";
+import './aiGenerator.css'
 
 const AiGenerator = () => {
   const dispatch = useDispatch();
@@ -98,17 +99,21 @@ const AiGenerator = () => {
 
 
   return (
-    <div className="flex flex-col justify-center items-center h-[94vh] bg-gradient-to-r from-blue-500 to-purple-500" style={isLoading ? { opacity: 0.5 } : { opacity: 1 }}>
+    <div className="flex flex-col justify-center items-center h-[94vh] bg-gradient-to-br from-gray-900 to-black" style={isLoading ? { opacity: 0.5 } : { opacity: 1 }}>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
-      <div style={{ position: 'fixed', left: '4vh', top: '8vh', background:'white', padding:'1.1rem', borderRadius:'15px', fontSize:'1.5rem', fontWeight:'bold', }}>Balance Credits : {balanceCredits}</div>
-      <div className="bg-white p-10 rounded-lg shadow-lg" style={{ maxWidth: "75vw" }}>
+      <div style={{ position: 'fixed', left: '2vh', top: '8vh', background:'white', padding:'1.1rem', borderRadius:'15px', fontSize:'1.5rem', fontWeight:'bold', }}>Balance Credits : {balanceCredits}
+      </div>
+      <div className="shadows" style={{ maxWidth: "75vw"}}>
         <div className="mb-[7rem]">
           <h1 className="text-3xl font-bold">Generate Your Reports Effortlessly.</h1>
           <h4 className="text-[1rem]">- With the power of AI</h4>
         </div>
 
+           
+
+
         <div className="mb-14 flex flex-col" style={{ justifyContent: 'flex-start' }}>
-          <label htmlFor="input" className="block text-lg font-semibold font-medium text-gray-700">
+          <label htmlFor="input" className="block text-lg font-semibold font-medium">
             Enter your website / (sub) domain / host / sdk name:
           </label>
           <input
@@ -116,7 +121,7 @@ const AiGenerator = () => {
             rows="4"
             onChange={(event) => handleOtherDetailsChange(event.target.value)}
             style={{ border: '1px solid gray' }}
-            className="mt-1 block rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-[2rem] p-4"
+            className="mt-1 block rounded-md  sm:text-sm h-[2rem] p-4 bg-gradient-to-br from-gray-900 to-black text-white"
           ></input>
         </div>
         <div className="mb-14">
@@ -126,9 +131,7 @@ const AiGenerator = () => {
               <button
                 key={vulnerability.value}
                 onClick={() => handleVulnerabilitySelect(vulnerability)}
-                className={`border border-gray-300 rounded-md px-3 py-1 text-sm ${selectedVulnerability === vulnerability.value
-                    ? "bg-blue-500 text-white"
-                    : "hover:bg-gray-100"
+                className={`item ${selectedVulnerability === vulnerability.value ? 'selected' : ''}
                   }`}
               >
                 {vulnerability.label}
@@ -144,7 +147,9 @@ const AiGenerator = () => {
               <button
                 key={heading.value}
                 onClick={() => handleHeadingToggle(heading.value)}
-                className={`border border-gray-300 rounded-md px-3 py-1 text-sm ${selectedHeadings.includes(heading.value) ? "bg-blue-500 text-white" : "hover:bg-gray-100"}`}
+                // className={`border border-gray-300 rounded-md px-3 py-1 text-sm ${selectedHeadings.includes(heading.value) ? "bg-blue-500 text-white" : "hover:bg-gray-100"}`}
+                className={`item ${selectedHeadings.includes(heading.value)? 'selected' : ''}
+                  }`}
               >
                 {heading.label}
               </button>
@@ -153,14 +158,14 @@ const AiGenerator = () => {
         </div>
 
         <div className="mb-4">
-          <label htmlFor="input" className="block text-lg font-semibold font-medium text-gray-700">
+          <label htmlFor="input" className="block text-lg font-semibold font-medium ">
             If the above options doesn't satisfy you, explain the vulnerability , along with it's details to generate your report :
           </label>
           <textarea
             id="input"
             rows="4"
             onChange={(event) => handleOtherDetailsChange(event.target.value)}
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm  bg-gradient-to-br from-gray-900 to-black text-white"
           ></textarea>
         </div>
         <button
